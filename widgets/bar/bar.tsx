f702b3c3bea6d70b87275cdd0b44e1499ebe8079
Hansen <samhansen.dev@gmail.com>
2025-01-28 19:23:25 +0300

    feat(bar): add launcher button

--- widgets/bar/bar.tsx
+++ widgets/bar/bar.tsx
@@ -1,4 +1,5 @@
 import { App, Astal, Gtk, type Gdk } from "astal/gtk3"
+import LauncherButton from "./modules/launcherbutton"
 import WorkSpaces from "./modules/workspaces"
 // import Media from "./modules/media"
 import Time from "./modules/time"
@@ -8,6 +9,7 @@ import Media from "./modules/media"
 
 const AnchorLeft = (
   <box spacing={8} hexpand halign={Gtk.Align.START}>
+    <LauncherButton />
     <WorkSpaces />
   </box>
 );
