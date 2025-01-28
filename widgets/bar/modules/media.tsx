8816876842301213bce7d8de1e608b9a9e29fd3d
Hansen <samhansen.dev@gmail.com>
2025-01-28 23:28:06 +0300

    chore(player): need fix asap

--- widgets/bar/modules/media.tsx
+++ widgets/bar/modules/media.tsx
@@ -1,19 +1,23 @@
-import { Gtk } from "astal/gtk3"
-import { bind } from "astal"
-import Mpris from "gi://AstalMpris"
+import { Gtk } from "astal/gtk3";
+import { App } from "astal/gtk3";
+import { bind } from "astal";
+import Mpris from "gi://AstalMpris";
+import { WINDOW_NAME as MEDIA_PLAYER } from "../../player/player";
 
 export default function Media() {
   const mpris = Mpris.get_default()
 
   return <box className="Media">
     {bind(mpris, "players").as(ps => ps[0] ? (
-      <box>
-        <box
+      <box
+        className="mpris_onbar"
+      // onClick={() => App.toggle_window(MEDIA_PLAYER)}
+      >
+        <button
           className="Cover"
           valign={Gtk.Align.CENTER}
-          css={bind(ps[0], "coverArt").as(cover =>
-            `background-image: url('${cover}');`
-          )}
+          label="♪"
+          onClick={() => App.toggle_window(MEDIA_PLAYER)}
         />
         <label
           label={bind(ps[0], "title").as(() =>
