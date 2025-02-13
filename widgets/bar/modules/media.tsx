bbb9e861c8faafd8ed036f207e926107f8a5df03
Hansen <sammhansen@gmail.com>
2025-02-13 20:35:16 +0300

    todo: fix unknown track thingy

--- widgets/bar/modules/media.tsx
+++ widgets/bar/modules/media.tsx
@@ -1,34 +1,41 @@
-import { Gtk, App } from "astal/gtk3";
+import { Gtk, App } from "astal/gtk4";
 import { bind } from "astal";
 import Mpris from "gi://AstalMpris";
-import { WINDOW_NAME as MEDIA_PLAYER } from "../../player/player";
+// import { WINDOW_NAME as MEDIA_PLAYER } from "../../player/player";
 
 export default function Media() {
   const mpris = Mpris.get_default();
 
   return (
-    <eventbox
-      className="Media"
-      onClick={() => {
-        App.toggle_window(MEDIA_PLAYER);
-      }}
+    <box
+      cssName="Media"
+    // onClick={() => {
+    // App.toggle_window(MEDIA_PLAYER);
+    // }}
     >
       {bind(mpris, "players").as((ps) =>
         ps[0] ? (
-          <box className="mpris_onbar">
+          <box cssName="mpris_onbar">
             <label
               label={bind(ps[0], "title").as(() => {
                 const title = ps[0]?.title || "Unknown Track";
                 return title;
               })}
             />
-            <button className="Cover" valign={Gtk.Align.CENTER} label="♪" />
+            <button
+              cssName="Cover"
+              valign={Gtk.Align.CENTER}
+              label="♪"
+              onClicked={() => {
+                // App.toggle_window(MEDIA_PLAYER);
+              }}
+            />
           </box>
         ) : (
           ""
         )
       )}
-    </eventbox>
+    </box>
   );
 }
 
