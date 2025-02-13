52073d68a4311dd110b7b27eb523d67ff4a9476e
Hansen <sammhansen@gmail.com>
2025-02-13 20:35:48 +0300

    fix: minor changes

--- app.ts
+++ app.ts
@@ -1,17 +1,10 @@
-import { App } from "astal/gtk3"
+import { App } from "astal/gtk4"
 import style from "./scss/style.scss"
 import Bar from "./widgets/bar/bar"
-import NotificationPopups from "./widgets/notifications/notificationPopup"
-import OSD from "./widgets/osd/osd"
-import MprisPlayers from "./widgets/player/player"
 
-App.start
-  ({
-    css: style,
-    main() {
-      App.get_monitors().map(Bar)
-      OSD()
-      NotificationPopups()
-      MprisPlayers()
-    },
-  })
+App.start({
+  css: style,
+  main() {
+    App.get_monitors().map(Bar)
+  },
+})
--- widgets/bar/bar.tsx
+++ widgets/bar/bar.tsx
@@ -1,14 +1,20 @@
-import { App, Astal, Gtk, type Gdk } from "astal/gtk3"
+import { App, Astal, Gtk, Gdk } from "astal/gtk4"
+import { Variable } from "astal"
 import LauncherButton from "./modules/launcherbutton"
-import WorkSpaces from "./modules/workspaces"
-// import Media from "./modules/media"
 import Time from "./modules/time"
-import SysTray from "./modules/systray"
-import BatteryLevel from "./modules/battery"
 import Media from "./modules/media"
+import WorkSpaces from "./modules/workspaces"
+import SystemTray from "./modules/systray"
+import BatteryLevel from "./modules/battery"
+
+const time = Variable("").poll(1000, "date")
 
 const AnchorLeft = (
-  <box spacing={8} hexpand halign={Gtk.Align.START}>
+  < box
+    spacing={8}
+    hexpand
+    halign={Gtk.Align.START}
+  >
     <LauncherButton />
     <Time />
     <Media />
@@ -16,33 +22,42 @@ const AnchorLeft = (
 );
 
 const AnchorCenter = (
-  <box spacing={8}>
+  <box
+    spacing={8}
+    hexpand
+    halign={Gtk.Align.CENTER}
+  >
     <WorkSpaces />
   </box>
 );
 
 const AnchorRight = (
-  <box spacing={8} hexpand halign={Gtk.Align.END}>
-    <SysTray />
+  <box
+    spacing={8}
+    hexpand
+    halign={Gtk.Align.END}
+  >
+    <SystemTray />
     <BatteryLevel />
   </box>
 );
 
-export default function Bar(monitor: Gdk.Monitor) {
+export default function Bar(gdkmonitor: Gdk.Monitor) {
   const { TOP, LEFT, RIGHT } = Astal.WindowAnchor
 
   return <window
-    className="Bar"
-    gdkmonitor={monitor}
+    visible
+    cssClasses={["Bar"]}
+    gdkmonitor={gdkmonitor}
     exclusivity={Astal.Exclusivity.EXCLUSIVE}
     anchor={TOP | LEFT | RIGHT}
+    application={App}
   >
     <box
-      className="box_bg"
-    // heightRequest={60}
-    // widthRequest={800}
+      cssName="box_bg"
     >
       <centerbox
+        hexpand
         cssName="centerbox"
         start_widget={AnchorLeft}
         center_widget={AnchorCenter}
