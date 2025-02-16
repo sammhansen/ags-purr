d6da9a4637286edc46f103d00f5e37e8d902584e
Hansen <sammhansen@gmail.com>
2025-02-16 17:07:56 +0300

    refactor: minor changes

--- app.ts
+++ app.ts
@@ -1,10 +1,12 @@
 import { App } from "astal/gtk4"
 import style from "./scss/style.scss"
 import Bar from "./widgets/bar/bar"
+import { OSD } from "./widgets/osd/osd"
 
 App.start({
   css: style,
   main() {
     App.get_monitors().map(Bar)
+    // OSD()
   },
 })
