2d2cf875c3831d3e6cb24950386ea89cf374ff01
Hansen <sammhansen@gmail.com>
2025-02-16 09:17:25 +0300

    fix: system tray

--- widgets/bar/modules/systray.tsx
+++ widgets/bar/modules/systray.tsx
@@ -1,20 +1,33 @@
-import { bind } from "astal"
-import AstalTray from "gi://AstalTray"
+import { bind } from "astal";
+import { hook } from "astal/gtk4";
+import AstalTray from "gi://AstalTray";
 
 export default function SysTray() {
-  const tray = AstalTray.get_default()
+  const tray = AstalTray.get_default();
 
-  return <box cssName="SysTray">
-    {bind(tray, "items").as(items => items.map(item => (
-      <menubutton
-        tooltipMarkup={bind(item, "tooltipMarkup")}
-        // usePopover={false}
-        actionGroup={bind(item, "actionGroup").as(ag => ["dbusmenu", ag])}
-        menuModel={bind(item, "menuModel")}
-      >
-        <image gicon={bind(item, "gicon")} />
-      </menubutton>
-    )))}
-  </box>
+  return (
+    <box cssName="SysTray">
+      {bind(tray, "items").as(items =>
+        items.map(item => (
+          <menubutton
+            tooltipMarkup={bind(item, "tooltipMarkup")}
+            menuModel={bind(item, "menuModel")}
+            setup={self => {
+              hook(self, item, "notify::action-group", () => {
+                if (item.action_group) {
+                  self.insert_action_group("dbusmenu", item.action_group);
+                }
+              });
+              if (item.action_group) {
+                self.insert_action_group("dbusmenu", item.action_group);
+              }
+            }}
+          >
+            <image gicon={bind(item, "gicon")} />
+          </menubutton>
+        ))
+      )}
+    </box>
+  );
 }
 
