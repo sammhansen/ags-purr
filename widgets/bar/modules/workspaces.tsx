a80b997be98b8de2571c8ad949381c7dbd94cbc5
Hansen <sammhansen@gmail.com>
2025-02-16 16:19:33 +0300

    feat: show workspaces as ranges

--- widgets/bar/modules/workspaces.tsx
+++ widgets/bar/modules/workspaces.tsx
@@ -1,32 +1,32 @@
 import Hyprland from "gi://AstalHyprland";
 import { bind } from "astal";
-import { Gtk } from "astal/gtk4";
 
 export default function WorkSpaces() {
   const hypr = Hyprland.get_default();
 
   return (
     <box cssName="workspaces">
-      {bind(hypr, "workspaces").as((ws) => {
-        return [...Array(10)]
-          .map((_, i) => i + 1)
-          .map((index) => (
-            <button
-              // valign={Gtk.Align.CENTER}
-              vexpand={false}
-              onClicked={() => hypr.dispatch("workspace", index.toString())}
-              cssClasses={bind(hypr, "focusedWorkspace").as((focused) => [
-                focused.id === index
-                  ? "focused"
-                  : ws.find((w) => w.id === index)
-                    ? "occupied"
-                    : "default",
-              ])}
-            >
-              {index}
-            </button>
-          ));
+      {bind(hypr, "focusedWorkspace").as((focused) => {
+        const start = Math.floor((focused.id - 1) / 10) * 10 + 1;
+        const workspaces = Array.from({ length: 10 }, (_, i) => start + i);
+
+        return workspaces.map((index) => (
+          <button
+            vexpand={false}
+            onClicked={() => hypr.dispatch("workspace", index.toString())}
+            cssClasses={bind(hypr, "workspaces").as((ws) => [
+              focused.id === index
+                ? "focused"
+                : ws.some((w) => w.id === index)
+                  ? "occupied"
+                  : "default",
+            ])}
+          >
+            {index}
+          </button>
+        ));
       })}
     </box>
   );
 }
+
