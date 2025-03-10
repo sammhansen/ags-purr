315f945cde206bf9962b5cb8a57c2e8eceb1161c
Hansen <samhansen.dev@gmail.com>
2025-03-10 23:00:38 +0300

    fix: launcher button icon

--- widgets/bar/modules/launcherbutton.tsx
+++ widgets/bar/modules/launcherbutton.tsx
@@ -1,37 +1,12 @@
-import { App, Gtk } from "astal/gtk3";
-import { Player } from "./../../player/Main";
-import { MaterialIcon } from "../../custom/MaterialIcon";
-
 export default function LauncherButton(): JSX.Element {
-	return (
-		<box
-			vertical
-			// widthRequest={200}
-			className="Battery"
-		// visible={bind(bat, "isPresent")}
-		>
-			<box hexpand />
-			<box
-				className="batt-container"
-				hexpand
-				halign={Gtk.Align.CENTER}
-				heightRequest={40}
-				widthRequest={40}
-			>
-				<button
-					className="batt-bar"
-					hexpand={true}
-					onClicked={() => {
-						App.toggle_window(Player);
-					}}
-					valign={Gtk.Align.END} // Align to bottom
-					heightRequest={20}
-					widthRequest={20}
-					child=<MaterialIcon icon="grass" size="5" />
-				/>
-			</box>
-			<box hexpand />
-		</box>
-	);
-
+	return <button
+		marginTop={3}
+		className="launcher_button"
+		onClicked="anyrun"
+	>
+		<label
+			className="launcher_label"
+			label=" "
+		/>
+	</button>
 }
